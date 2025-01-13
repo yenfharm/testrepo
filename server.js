54c543fd2df300ae3e9a7aa94838c378b79d66b6
yenfharm <yenfhael.arm@gmail.com>
2025-01-13 17:03:05 -0400

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
+// const static = require("./routes/static")
 
 
 /* ***********************
